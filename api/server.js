1a9364d95613e9ea7efcbbba7d7d7f7efdd33584
Jamison Fox <fox.jamison@outlook.com>
2023-02-27 13:24:46 -0700

    configured and made testable. completed Users.find

--- api/server.js
+++ api/server.js
@@ -14,6 +14,10 @@ server.use(cors());
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
 
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'api up and running' })
+})
+
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
